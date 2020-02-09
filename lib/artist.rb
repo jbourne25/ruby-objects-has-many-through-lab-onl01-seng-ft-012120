4c4a43d5e8e17ef0a219ab5684cdfd31ef31042b
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-09 01:55:56 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def genres 
+    songs.collect{ |song| song.genre }
+  end 
+  
+  
+  def songs
+    Song.all.select{ |song| song.artist == self }
+  end 
+  
+  def new_song(song, genre) 
+    Song.new(song, self, genre)
+  end 
+  
+  
+  
+  
+  
+end 
